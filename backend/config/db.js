614d06832bb1f16b1c337ff2d775fedda007b185
dag317 <avdahana@gmail.com>
2026-04-26 12:46:03 +0300

    Change host

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 import mysql from 'mysql2/promise';
 
 export const db = mysql.createPool({
-  host: 'localhost',
+  host: '127.0.0.1',
   user: 'root',
   password: 'AN0660193s',
   database: 'scripto_db'
