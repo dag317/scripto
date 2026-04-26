a06ecffc574008be6cc4b149546731bbe570a7ff
dag317 <avdahana@gmail.com>
2026-04-26 13:28:05 +0300

    server fixes

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,5 +4,10 @@ export const db = mysql.createPool({
   host: '127.0.0.1',
   user: 'root',
   password: 'AN0660193s',
-  database: 'scripto_db'
+  database: 'scripto_db',
+  waitForConnections: true,   
+  connectionLimit: 10,         
+  queueLimit: 0,             
+  enableKeepAlive: true,     
+  keepAliveInitialDelay: 10000
 });
